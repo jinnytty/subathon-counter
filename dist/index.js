7b3768c3fc652677feba8250caaf9ac88ceab9e3
m4eba <m4eba.0@gmail.com>
2022-08-11 09:43:55 +0200

    Add counter logic

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,71 @@
+import fs from 'fs';
 import { parse } from 'ts-command-line-args';
+import { Duration } from 'luxon';
 import { Streamlabs, StreamlabsConfigOpt, } from './streamlabs.js';
 import { Toon, ToonConfigOpt } from './toonation.js';
 import { Twip, TwipConfigOpt } from './twip.js';
+import { Twitch, TwitchConfigOpt } from './twitch.js';
 const CounterConfigOpt = {
     config: { type: String, optional: true },
+    donoRateFile: { type: String, defaultValue: 'dono.json' },
+    subRateFile: { type: String, defaultValue: 'sub.json' },
+    startTimer: { type: String, optional: true },
 };
 const config = parse({
     ...TwipConfigOpt,
     ...ToonConfigOpt,
     ...StreamlabsConfigOpt,
+    ...TwitchConfigOpt,
     ...CounterConfigOpt,
 }, {
     loadFromFileArg: 'config',
 });
+const donoRate = JSON.parse(await fs.promises.readFile(config.donoRateFile, { encoding: 'utf8' }));
+const subRate = JSON.parse(await fs.promises.readFile(config.subRateFile, { encoding: 'utf8' }));
+let last = new Date().getTime();
+let lastOutput = '';
+let timer = Duration.fromMillis(0);
+if (config.startTimer) {
+    timer = Duration.fromISOTime(config.startTimer);
+}
+setInterval(() => {
+    const time = new Date().getTime();
+    //const diff = Math.min(time - last, timer.toMillis());
+    const diff = time - last;
+    last = time;
+    timer = timer.minus(Duration.fromMillis(diff));
+    update();
+}, 250);
+function update() {
+    if (timer.valueOf() < 0) {
+        timer = Duration.fromMillis(0);
+    }
+    const output = timer.toFormat('hh:mm:ss');
+    if (output !== lastOutput) {
+        lastOutput = output;
+        console.log('Timer:', output);
+    }
+}
 const donation = (donation) => {
     console.log('donation:', donation.amount, donation.currency);
+    updateTime(donoRate, donation.currency, donation.amount);
 };
+const subscription = (sub) => {
+    console.log('subscription', sub.plan);
+    updateTime(subRate, sub.plan, 1);
+};
+function updateTime(rates, key, amount) {
+    key = key.toLowerCase();
+    if (rates[key]) {
+        timer = timer.plus(Duration.fromObject({
+            seconds: amount * rates[key],
+        }));
+        update();
+    }
+    else {
+        console.log('key not found!!!!', key, rates);
+    }
+}
 if (config.twipToken.length > 0) {
     const twip = await Twip.create(config);
     twip.onDonation(donation);
@@ -28,3 +78,6 @@ if (config.streamlabsSocketToken.length > 0) {
     const streamlabs = await Streamlabs.create(config);
     streamlabs.onDonation(donation);
 }
+const twitch = await Twitch.create(config);
+twitch.onDonation(donation);
+twitch.onSubscription(subscription);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import fs from 'fs';
 import { ArgumentConfig, parse } from 'ts-command-line-args';
-import type { DonationCallback, DonationMessage } from './model.js';
+import { Duration } from 'luxon';
+import type {
+  DonationCallback,
+  DonationMessage,
+  SubscriptionCallback,
+  SubscriptionMessage,
+} from './model.js';
 import {
   Streamlabs,
   StreamlabsConfig,
@@ -7,19 +14,27 @@ import {
 } from './streamlabs.js';
 import { Toon, ToonConfig, ToonConfigOpt } from './toonation.js';
 import { Twip, TwipConfig, TwipConfigOpt } from './twip.js';
+import { Twitch, TwitchConfig, TwitchConfigOpt } from './twitch.js';
 
 interface CounterConfig {
   config?: string;
+  donoRateFile: string;
+  subRateFile: string;
+  startTimer?: string;
 }
 
 const CounterConfigOpt: ArgumentConfig<CounterConfig> = {
   config: { type: String, optional: true },
+  donoRateFile: { type: String, defaultValue: 'dono.json' },
+  subRateFile: { type: String, defaultValue: 'sub.json' },
+  startTimer: { type: String, optional: true },
 };
 
 interface Config
   extends TwipConfig,
     ToonConfig,
     StreamlabsConfig,
+    TwitchConfig,
     CounterConfig {}
 
 const config: Config = parse<Config>(
@@ -27,6 +42,7 @@ const config: Config = parse<Config>(
     ...TwipConfigOpt,
     ...ToonConfigOpt,
     ...StreamlabsConfigOpt,
+    ...TwitchConfigOpt,
     ...CounterConfigOpt,
   },
   {
@@ -34,9 +50,68 @@ const config: Config = parse<Config>(
   }
 );
 
+const donoRate = JSON.parse(
+  await fs.promises.readFile(config.donoRateFile, { encoding: 'utf8' })
+);
+const subRate = JSON.parse(
+  await fs.promises.readFile(config.subRateFile, { encoding: 'utf8' })
+);
+
+let last = new Date().getTime();
+let lastOutput = '';
+let timer = Duration.fromMillis(0);
+
+if (config.startTimer) {
+  timer = Duration.fromISOTime(config.startTimer);
+}
+
+setInterval(() => {
+  const time = new Date().getTime();
+  //const diff = Math.min(time - last, timer.toMillis());
+  const diff = time - last;
+  last = time;
+  timer = timer.minus(Duration.fromMillis(diff));
+  update();
+}, 250);
+
+function update() {
+  if (timer.valueOf() < 0) {
+    timer = Duration.fromMillis(0);
+  }
+  const output = timer.toFormat('hh:mm:ss');
+  if (output !== lastOutput) {
+    lastOutput = output;
+    console.log('Timer:', output);
+  }
+}
+
 const donation: DonationCallback = (donation: DonationMessage) => {
   console.log('donation:', donation.amount, donation.currency);
+  updateTime(donoRate, donation.currency, donation.amount);
 };
+
+const subscription: SubscriptionCallback = (sub: SubscriptionMessage) => {
+  console.log('subscription', sub.plan);
+  updateTime(subRate, sub.plan, 1);
+};
+
+function updateTime(
+  rates: { [key: string]: number },
+  key: string,
+  amount: number
+): void {
+  key = key.toLowerCase();
+  if (rates[key]) {
+    timer = timer.plus(
+      Duration.fromObject({
+        seconds: amount * rates[key],
+      })
+    );
+    update();
+  } else {
+    console.log('key not found!!!!', key, rates);
+  }
+}
 
 if (config.twipToken.length > 0) {
   const twip = await Twip.create(config);
@@ -52,3 +127,7 @@ if (config.streamlabsSocketToken.length > 0) {
   const streamlabs = await Streamlabs.create(config);
   streamlabs.onDonation(donation);
 }
+
+const twitch = await Twitch.create(config);
+twitch.onDonation(donation);
+twitch.onSubscription(subscription);
